bb350b1de5b5c63e0542a22abd3cffeed73de539
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-06-10 15:17:06 -0500

    removed fragment, imported puppyList from data.js, and added map filter + jsx

--- puppy-workshop-24/src/App.jsx
+++ puppy-workshop-24/src/App.jsx
@@ -1,15 +1,19 @@
+import {puppyList} from './data.js'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
 
   return (
-    <>
-      <div>
-</div>
-    </>
-  )
+      <div className="App">
+        {
+          puppies.map((puppy) => { 
+            return <p key={puppy.id}>{puppy.name}</p> 
+          })
+        }
+    </div>
+  );
 }
 
 export default App
