949001caade3282841178d0d13140390f560598d
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-06-12 14:49:43 -0500

    added img tag for featured pup images, added className, modified return statement

--- puppy-workshop-24/src/App.jsx
+++ puppy-workshop-24/src/App.jsx
@@ -1,39 +1,43 @@
-import {puppyList} from './data.js'
-import { useState } from 'react'
-import './App.css'
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setFeatPupId] = useState(null)
-  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
-
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
-      <div className="App">
-        {puppies.map((puppy) => { 
-            return <p onClick = {() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p> 
-            
-          })
-        }
-        {
-          
-        featPupId && (
-        <div>
+    <div className="App">
+      <h1>Puppy Pals</h1>
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div className="puppyDetails">
           <h2>{featuredPup.name}</h2>
+          <img src={featuredPup.image} alt="dog" />
           <ul>
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
         </div>
-        )}
+      )}
     </div>
   );
 }
 
-
-export default App
+export default App;
 
 // Notes
 
 // null is used to conditionally render an element in JSX code.
 // featPupID is a state variable: used to retain data between renders
-// setFeatPupID is the setter function: used to update the variable + trigger react to render the component again 
+// setFeatPupID is the setter function: used to update the variable + trigger react to render the component again
